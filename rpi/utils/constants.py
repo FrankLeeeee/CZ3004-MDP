230b8b33e704c60da60a868e9bb471153136c641
Li Yuanming <yli056@e.ntu.edu.sg>
2021-02-25 22:51:38 +0800

    :wrench: Update message protocol for serial communication

--- rpi/utils/constants.py
+++ rpi/utils/constants.py
@@ -1,3 +1,16 @@
 from pathlib import Path
 
 PROJECT_ROOT_PATH = Path(__file__).parents[1].absolute()
+
+# Protocol version
+VERSION = 1
+
+# Session
+SESSION_LENGTH = 1
+
+DATA_LEN_LENGTH = 4
+# VERSION (1) + DATA LEN (4) + SESSION_CODE_LENGTH CODE (1)
+HEADER_LENGTH = 1 + DATA_LEN_LENGTH + SESSION_LENGTH
+
+BYTE_ORDER = 'little'
+SEPARATOR = b'\x12\x0c'
