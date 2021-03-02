b478bed18206b65db18fe3ee843382d0f3c7a028
Li Yuanming <yli056@e.ntu.edu.sg>
2021-03-02 14:32:03 +0800

    :wrench: Fix serial communication

--- rpi/core/arduino_service_pb2_serial.py
+++ rpi/core/arduino_service_pb2_serial.py
@@ -1,6 +1,4 @@
-import json
-
-import google.protobuf.json_format
+import struct
 
 import core.message_pb2 as core_dot_message__pb2
 from server.serial_comm import SerialAioChannel
@@ -17,50 +15,70 @@ class ArduinoRPCServiceStub(object):
         """
 
         def deserializer_factory(message):
-            def _deserializer(text):
-                return google.protobuf.json_format.Parse(
-                    text=text,
-                    message=message()
-                )
+            def _deserializer(text: bytes):
+                args = text.split(b',')
+                assert len(args) >= 1
+                response = message(status=struct.unpack('?', args[-1])[0])
+
+                if message == core_dot_message__pb2.MetricResponse:
+                    assert len(args) == 2
+                    assert len(args[0]) == 24, 'Metric data should contain 24 bytes'
+
+                    for i in range(24 // 4):
+                        response.value[i + 1] = struct.pack('f', args[0])
+                    return response
+                elif message == core_dot_message__pb2.EchoResponse:
+                    assert len(args) == 2
+                    response.message = args[0]
+                    return response
+                else:
+                    assert len(args) == 1
+                    return response
             return _deserializer
 
-        def serializer(message):
-            data = google.protobuf.json_format.MessageToDict(message, including_default_value_fields=True)
-            return json.dumps(data)
+        def default_serializer(message):
+            data = list()
+            for field in message.DESCRIPTOR.fields:
+                value = getattr(message, field.name)
+                if isinstance(value, bytes):
+                    data.append(value)
+                else:
+                    data.append(bytes(data))
+            return b','.join(data)
 
         # request_serializer, response_deserializer
         self.Echo = channel.unary_unary(
-            'Echo',
-            request_serializer=serializer,
+            b'\x01',
+            request_serializer=default_serializer,
             response_deserializer=deserializer_factory(core_dot_message__pb2.EchoResponse),
         )
         self.Forward = channel.unary_unary(
-            'Forward',
-            request_serializer=serializer,
+            b'\x02',
+            request_serializer=default_serializer,
             response_deserializer=deserializer_factory(core_dot_message__pb2.MetricResponse),
         )
         self.TurnLeft = channel.unary_unary(
-            'TurnLeft',
-            request_serializer=serializer,
+            b'\x03',
+            request_serializer=default_serializer,
             response_deserializer=deserializer_factory(core_dot_message__pb2.MetricResponse),
         )
         self.TurnRight = channel.unary_unary(
-            'TurnRight',
-            request_serializer=serializer,
+            b'\x04',
+            request_serializer=default_serializer,
             response_deserializer=deserializer_factory(core_dot_message__pb2.MetricResponse),
         )
         self.GetMetrics = channel.unary_unary(
-            'GetMetrics',
-            request_serializer=serializer,
+            b'\x05',
+            request_serializer=default_serializer,
             response_deserializer=deserializer_factory(core_dot_message__pb2.MetricResponse),
         )
         self.Calibration = channel.unary_unary(
-            'Calibration',
-            request_serializer=serializer,
+            b'\x06',
+            request_serializer=default_serializer,
             response_deserializer=deserializer_factory(core_dot_message__pb2.Status),
         )
         self.Terminate = channel.unary_unary(
-            'Terminate',
-            request_serializer=serializer,
+            b'\x07',
+            request_serializer=default_serializer,
             response_deserializer=deserializer_factory(core_dot_message__pb2.Status),
         )
--- rpi/utils/constants.py
+++ rpi/utils/constants.py
@@ -4,5 +4,4 @@ PROJECT_ROOT_PATH = Path(__file__).parents[1].absolute()
 
 METHOD_LENGTH = 1
 BYTE_ORDER = 'little'
-SEPARATOR = ';'
-COMMAND_SEPARATOR = '\\'
+SEPARATOR = b';'
